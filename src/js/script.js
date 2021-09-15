4237f6b305ba1c3f45bec558afe614bdabca74b7
Henrique <henriquegbritto2012@gmail.com>
2021-09-15 18:14:59 -0300

    Implementação do Slick para o slider iniciada*
    *Bugs para corrigir:
     - Posicionamento da nav
    
    *Faltam fazer: 
     - Mudar o indice na nav lateral de acordo com o banner correspondente
    
     - Mudar o nome de acordo com o banner

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,5 @@
 const btn = document.querySelectorAll('.buy')
 
-
-
 const addClass = function(){
     this.innerText = 'COMPRADO!'
     this.classList.add('bought')
@@ -10,4 +8,21 @@ const addClass = function(){
 
 btn.forEach((item)=>{
     item.addEventListener('click', addClass)
+})
+
+const banners = document.querySelectorAll('.banner-info')
+
+$(()=>{
+    $('.banners').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.nav-previous'),
+        nextArrow: $('.nav-next')
+    })
+
+    $('.banners').on('afterChange', ()=>{
+        $('.banner-info').toggleClass('active')
+        $('.nav-game-name').text('RED DEAD 2')
+    })
+
 })
